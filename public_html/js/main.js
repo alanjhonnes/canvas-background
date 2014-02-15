f08f2d639014b13347c094dc0f97293db6ac38d4
unknown <bianca.arodrigues@CAS382568J47713.senacsp.edu.br>
2014-02-15 11:38:42 -0200

    first draft

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,37 +1,92 @@
 // @see http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+
 window.requestAnimFrame = (function() {
-  return  window.requestAnimationFrame ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame ||
-          window.oRequestAnimationFrame ||
-          window.msRequestAnimationFrame ||
-          function(/* function */callback, /* DOMElement */element) {
-            window.setTimeout(callback, 1000 / 60);
-          };
+    return  window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(callback) {
+                window.setInterval(callback, 1000 / 60);
+            };
 })();
 
 
-$( window ).load(function() {
-  //Create a stage by getting a reference to the canvas
-    stage = new createjs.Stage("canvas1");
-    //Create a Shape DisplayObject.
-    circle = new createjs.Shape();
-    
-    g = new createjs.Graphics();
-    
-    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-    
-    //Set position of Shape instance.
-    circle.x = circle.y = 50;
-    //Add Shape instance to stage display list.
-    stage.addChild(circle);
-    //Update stage will render next frame
-    stage.update();
 
-    $(window).resize(function(){
-      
+
+$(window).load(function() {
+
+    var stageWidth, stageHeight;
+	var $canvas = $("#canvas1");
+	
+	//Create a stage by getting a reference to the canvas
+    
+    var stage = new createjs.Stage("canvas1");
+    
+    var lines = new createjs.Shape();
+	stage.addChild(lines);
+	
+	var redrawLines = function(){
+		//Create a Shape DisplayObject.
+	
+	    var g = lines.graphics;
+	    
+	    g.clear();
+	
+	    var numLines = 10;
+	
+	    var xOffset = 150;
+		g.setStrokeStyle(1);
+	    g.beginStroke("#ffffff");
+	 	var xPosition = 0;
+	    for (var i = 0; i < numLines; i++) {
+	        g.lineTo(xPosition, stageHeight);
+	        xPosition += xOffset;
+	        g.moveTo(xPosition, 0);
+	    }
+	};
+    
+    $(window).resize(function() {
+        stageWidth = $(window).width();
+        stageHeight = $(window).height();
+        $canvas.attr('width', stageWidth);
+        $canvas.attr('height', stageHeight);
+        $canvas.css('width', stageWidth + "px");
+        $canvas.css('height', stageHeight + "px");
+    	//stage.canvas.width = window.innerWidth;
+        //stage.canvas.height = window.innerHeight; 
+        redrawLines();
+        stage.update();
     }).resize();
+    
+    //redrawLines();
+    
+	$("body").mousemove(function( event ) {
+	  var mouseRatioX = event.pageX / stageWidth;
+	  
+	  TweenMax.to(lines, 1, { x: -mouseRatioX * 150} );
+	  
+	  //stage.update();
+	});
+	
+    
+    
 
-  //requestAnimFrame(update);
+
+
+    
+
+	var update = function(){
+		stage.update();
+		console.log('updating');
+	};
+	
+	window.setInterval(update, 1000 / 60);
+	
+	//requestAnimFrame(update);
+	
+	
+
+    //requestAnimFrame(update);
 
 });
